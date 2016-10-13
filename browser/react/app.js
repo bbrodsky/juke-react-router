df0288e69fa97f20feeed0e6668bb152823418b9
dfrho <rhodesdav@gmail.com>
2016-10-13 15:44:32 -0400

    indexroute working

--- browser/react/app.js
+++ browser/react/app.js
@@ -4,11 +4,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import store from './store';
 import { Provider } from 'react-redux';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import AppContainer from './containers/AppContainer';
+//import Routes from './components/Routes';
+import Albums from './containers/AlbumsContainer';
+import Artists from './containers/ArtistsContainer';
+
+
 
 ReactDOM.render(
   <Provider store={store}>
-    <AppContainer /> 
+  	<Router history = {hashHistory}>
+    	<Route path='/' component={AppContainer}>
+	    	<Route path='/albums' component={Albums} />
+	    	<Route path='/artists' component={Artists} />
+	    	<IndexRoute component={Albums} />
+    	</Route>
+
+	</Router>
   </Provider>,
   document.getElementById('app')
 );
---
+++ browser/react/components/Routes.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import React from 'react';
+
+
+
+export default (props) => {
+
+	render () {
+	  return (
+	    <Router>
+	    	<Route path='/albums' component={Routes} />
+	    	<Route path='/artists' component={} />
+	    </Router>
+	  )
+
+}
+
